1aa99444f568e5cffe9dd8d00e83dcecd085d5c0
RajeevRanjan5642 <rajeevranjan31012003@gmail.com>
2024-07-08 12:27:33 +0530

    initial comit

---
+++ main.py
@@ -0,0 +1,29 @@
+# import version 3 of the imageio library
+import imageio.v3 as iio
+
+#  create a list of the image files
+filenames = ['./cats/cat-1.png','./cats/cat-2.png',
+             './cats/cat-3.png'];
+
+# create an empty list to store actual image data 
+# from these files
+images = [];
+
+# read the image using .imread() method into images 
+# list
+for filename in filenames:
+    images.append(iio.imread(filename));
+
+# use imwrite method to turn the images into a GIF
+iio.imwrite('cat.gif',images,duration=500,loop=0);
+
+# team.gif is name for the gif file to be created
+
+# duration means how long each picture should show 
+# in the gif in milliseconds
+
+# loop means how many times the GIF should repeat 
+# (0 means it keeps looping forever)
+
+
+
